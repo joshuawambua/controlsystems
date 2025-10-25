3b3dfc9dd87bb69ce3c7fad8d4702c07062c1cfd
joshuamuthenya <joshuawambua514@gmail.com>
2025-10-25 06:41:21 -0700

    submission with the simscape dc motor model complete

---
+++ simscape_dc_motor.m
@@ -0,0 +1,10 @@
+
+
+
+%SIMSCAPE DC MOTOR MODEL
+% joshua wambua
+J = 0.01;    % moment of inertia (kg*m^2)
+b = 0.1;     % friction coefficient (N*m*s)
+K = 0.01;    % motor constant (V/rad/s or N*m/A)
+R = 1;       % resistance (Ohm)
+L = 0.5;     % inductance (H)
